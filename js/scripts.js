16894ef32e31f6814eb5065788c976736af3b998
saraweinand <saraweinand@gmail.com>
2015-12-14 12:09:21 -0800

    12-14-15 12:09 pm

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-$('button').click(function() {
-  $(this).toggleClass('expanded').siblings('div').slideToggle();
+$('.menu-burger, .menu-items').on('click', function() {
+  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
 });
 
 
